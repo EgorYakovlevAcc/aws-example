38303067e274948acfd670f9a2b7d824e79e1be0
EgorYakovlevAcc <yakovlev.es@phystech.edu>
2021-07-15 18:30:43 +0300

    fix heroku deploy - 2

--- src/main/java/ru/aws/example/controller/ExampleController.java
+++ src/main/java/ru/aws/example/controller/ExampleController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.aws.example.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,8 +13,8 @@ import java.util.List;
 public class ExampleController {
     @GetMapping("/")
     public List<User> getUsers() {
-        List<User> users = List.of(new User(1, "Egor"));
-
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Egor"));
         return users;
     }
 }
